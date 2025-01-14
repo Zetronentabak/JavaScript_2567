daa1c9f769dfb68ceeb2524a54167723acf1adf4
Zetronentabak <tong.inmee@icloud.com>
2025-01-14 08:45:57 +0700

    com-ch5

---
+++ CH5/asynAwait.js
@@ -0,0 +1,21 @@
+function promiseTimeout(ms) {
+  return new Promise((resolve, reject) => {
+    setTimeout(resolve, ms);
+  });
+}
+
+async function longRunningOperation() {
+  return 42;
+}
+
+async function run() {
+  console.log("Start!!!");
+  await promiseTimeout(9000);
+
+  const response = await longRunningOperation();
+  console.log(response);
+  console.log("Stop!!!");
+}
+console.log("Before run");
+run();
+console.log("After run");
